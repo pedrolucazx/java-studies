8c2d1763351be0f585b101952e98dd8237d601c3
pedrolucazx <pedrolucazxmesquita@gmail.com>
2025-12-19 16:59:38 -0300

    feat: implement logic for job outcomes and TV purchase decision

--- challenges/src/Ex05_LogicOperation.java
+++ challenges/src/Ex05_LogicOperation.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Desafio Lógicos - Operadores Lógicos em Java
  *
@@ -23,6 +25,31 @@
 
 public class Ex05_LogicOperation {
     public static void main(String[] args) {
-        // Implemente sua solução conforme as instruções do desafio
+        boolean firstJob = false;
+        boolean secondJob = false;
+
+        boolean bothJobs = firstJob && secondJob;   // AND
+        boolean exactlyOne = firstJob ^ secondJob; // XOR
+        boolean anyJob = firstJob || secondJob;    // OR
+        boolean noJob = !anyJob;                   // NOT
+
+        int tvSize = bothJobs ? 50 : exactlyOne ? 32 : 0;
+        boolean iceCream = anyJob;
+
+        String jobsPhrase;
+        if (noJob) {
+            jobsPhrase = "Nenhum trabalho deu certo";
+        } else if (bothJobs) {
+            jobsPhrase = "Ambos os trabalhos deram certo";
+        } else if (firstJob) {
+            jobsPhrase = "O trabalho 1 deu certo";
+        } else {
+            jobsPhrase = "O trabalho 2 deu certo";
+        }
+
+        String icePhrase = iceCream ? "a família tomou sorvete" : "a família não tomou sorvete";
+        String tvPhrase = noJob ? "e não comprou TV" : "e comprou a TV de " + tvSize + " polegadas";
+
+        System.out.println(jobsPhrase + ", " + icePhrase + " " + tvPhrase + ".");
     }
 }
